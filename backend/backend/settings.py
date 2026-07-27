11c67dd5ac9a9a905d3d8c0591e10df5e82f616d
tabithakaruga <tabbykaruga130@gmail.com>
2026-07-27 12:15:17 +0300

    Backend and Frontend Connection of Products

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    "rest_framework",
+    "corsheaders",
     "base.apps.BaseConfig",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -117,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 function ProductScreen() {
     const { id } = useParams()
-    const product = products.find((p) => p._id == id)
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+        async function getProductById() {
+            const { data } = await axios.get(`/api/products/${id}`)
+            setProduct(data)
+        }
+        getProductById()
+    }, [id])
+
     return (
         <div>
             <Link to='/' className='btn btn-light my-3'>Go Back</Link>
