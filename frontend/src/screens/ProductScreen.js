9fa6b94f6108ab2448918efe314ba6dc958fd98c
tabithakaruga <tabbykaruga130@gmail.com>
2026-07-27 10:48:46 +0300

    Frontend and Backend Initialization

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
+
+function ProductScreen() {
+    const { id } = useParams()
+    const product = products.find((p) => p._id == id)
+    return (
+        <div>
+            <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item style={{ borderTop: '1px solid #dee2e6' }}>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+                        </ListGroup.Item>
+
+                        <ListGroup.Item style={{ borderTop: '1px solid #dee2e6' }}>
+                            Price : ${product.price}
+                        </ListGroup.Item>
+
+                        <ListGroup.Item style={{ borderTop: '1px solid #dee2e6' }}>
+                            Description : ${product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card style={{ overflow: 'hidden', border: '1px solid #dee2e6' }}>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col><strong>${product.price}</strong></Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item style={{ borderTop: '1px solid #dee2e6' }}>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item style={{ borderTop: '1px solid #dee2e6' }} >
+                                <Button className='w-100' type='button' disabled={product.countInStock === 0}>Add to Cart</Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default ProductScreen
