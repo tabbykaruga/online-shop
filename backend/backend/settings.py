afb843c24fe537852c883459177cf3eb29eea179
tabithakaruga <tabbykaruga130@gmail.com>
2026-09-21 13:41:43 +0300

    Change storage of images

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,9 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "cloudinary_storage",
     "django.contrib.staticfiles",
+    "cloudinary",
     "rest_framework",
     "corsheaders",
     "base.apps.BaseConfig",
@@ -158,11 +160,18 @@ STORAGES = {
     },
 }
 
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": config("CLOUDINARY_API_KEY"),
+    "API_SECRET": config("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_ROOT = BASE_DIR / "static" / "images"
 
 CORS_ALLOWED_ORIGINS = [
     "https://online-shop-jade-eta.vercel.app",
