dacdb9db00b2136b3e4dd9a3805378165d5f0203
tabithakaruga <tabbykaruga130@gmail.com>
2026-09-23 17:17:28 +0300

    Separating the products cards

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -29,7 +29,7 @@ function HomeScreen() {
     return (
       <div>
         {!keyword && <ProductCarousel />}
-        <Row>
+        <Row className="g-4">
           {products.map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product}></Product>
